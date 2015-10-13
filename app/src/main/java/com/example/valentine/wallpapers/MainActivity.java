3d029135caed1bc86eb271e71a8ea72b28f18f14
valentinerutto <rutto28@gmail.com>
2015-10-13 20:09:46 +0300

    created navigation drawer

--- app/src/main/java/com/example/valentine/wallpapers/MainActivity.java
+++ app/src/main/java/com/example/valentine/wallpapers/MainActivity.java
@@ -1,39 +1,68 @@
 package com.example.valentine.wallpapers;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
 
+    private int mutedColor;
 
+    private DrawerLayout drawerLayout;
+private Toolbar toolbar;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.shitstuff) ;
 
+//        mFragmentManager = getSupportFragmentManager();
+//        mFragmentTransaction = mFragmentManager.beginTransaction();
+//        mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+
+//                if (menuItem.getItemId() == R.id.nav_item_sent) {
+//                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+//                    fragmentTransaction.replace(R.id.containerView,  new TabFragment()).commit();
+//
+//                }
+//
+//                if (menuItem.getItemId() == R.id.nav_item_inbox) {
+//                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+//                    xfragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
+//                }
+
+                return false;
+            }
+
+        });
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar,R.string.app_name,
+                R.string.app_name);
+
+        drawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/valentine/wallpapers/TabFragment.java
@@ -0,0 +1,9 @@
+package com.example.valentine.wallpapers;
+
+import android.app.Fragment;
+
+/**
+ * Created by valentine on 10/13/15.
+ */
+public class TabFragment extends Fragment {
+}
